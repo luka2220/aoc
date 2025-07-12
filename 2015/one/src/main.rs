4d87a4c28001ea0abb8142106371fb162ccaea1d
Luka Piplica <42144047+luka2220@users.noreply.github.com>
2025-07-12 09:47:20 -0400

    2015-1-1

---
+++ 2015/one/src/main.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::{self, BufRead};
+
+const UP: char = '(';
+const DOWN: char = ')';
+
+fn main() -> io::Result<()> {
+    let f = File::open("input.txt").expect("Error reading file");
+    let reader = BufReader::new(f);
+
+    let mut parens = String::new();
+    let mut count = 0;
+
+    for line in reader.lines() {
+        parens = parens + &line?;
+    }
+
+    for (_idx, item) in parens.char_indices() {
+        match item {
+            UP => count += 1,
+            DOWN => count -= 1,
+            _ => continue,
+        }
+    }
+
+    println!("Santa needs to go to floor {count}");
+
+    Ok(())
+}
